c449103cb52032ef9fb19721fa2c2d7e5da0f6bc
Yi <ym2380@nyu.edu>
2024-09-27 17:18:36 -0400

    implemented batch gen and storing new gen data

--- pre_augment.py
+++ pre_augment.py
@@ -1,34 +1,134 @@
-### TODO: this is just a test. You need batch generating that could run for a long time and save them.
+# ==============================================================
+#                      _            _             _      
+#                     /\ \         /\ \     _    /\ \    
+#                    /  \ \       /  \ \   /\_\ /  \ \   
+#                   / /\ \ \     / /\ \ \_/ / // /\ \ \  
+#                  / / /\ \ \   / / /\ \___/ // / /\ \_\ 
+#                 / / /  \ \_\ / / /  \/____// /_/_ \/_/ 
+#                / / /   / / // / /    / / // /____/\    
+#               / / /   / / // / /    / / // /\____\/    
+#              / / /___/ / // / /    / / // / /______    
+#             / / /____\/ // / /    / / // / /_______\   
+#             \/_________/ \/_/     \/_/ \/__________/   
+#                                          
+# --------------------------------------------------------------
+#                 Project: News Unsupervised
+#                 Author: ONE
+# --------------------------------------------------------------
+#                 FILE: ./pre_augment.py
+# --------------------------------------------------------------
+#                          TODOs:
+#    1. Implement saving of generated data points
+# --------------------------------------------------------------
+#    Description:
+#        Generate augmented data points in advance
+# --------------------------------------------------------------
+# ==============================================================
+
+
+
+# packages
+import argparse
+
+import pandas
 
 from transformers import pipeline
 
+from data import News
+from utils import get_datetime_filename
 
 
 
+# args
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task_option", default="text-generation")
+    parser.add_argument("--model_name", default="gpt-3.5-turbo")
 
-def gen_augmentations(
+    parser.add_argument("--num_augmentations_per_news", type=int, default=6)
+    parser.add_argument("--max_length_per_generated", type=int, default=512)
+
+    args = parser.parse_args()
+    return args
+
+
+
+# generate fake news of a single news
+def gen_single_augmentations(
         prototype,
         generator,
         prompt,
+        #WARNING: not sure if max_length == 512 is appropriate
+        max_length=512,
         num_augmentations=6):
     prompt = prompt.format(prototype)
 
     augmenteds = []
-    for _ in range(num_augmentations):
-        augmented = generator(prompt, max_length=512, num_return_sequences=1)[0]['generated_text']
-        augmenteds.append(augmented)
+    raw_augmenteds = generator(prompt, max_length=max_length, num_return_sequences=num_augmentations)
+    for augmented in raw_augmenteds:
+        augmenteds.append(augmented["generated_text"])
+    
     return augmenteds
 
 
-if __name__ == "__main__":
-    generator = pipeline("text-generation", model="gpt-3.5-turbo")
-    prompt = f"Generate an article on the same topic as the following article: {}"
 
-    augmenteds = gen_augmentations(
-            prototype=article,
+# generate fake news of a batch of news (.csv)
+def gen_batch_augmentations(
+        args,
+        csv_path,
+        prototype_column_name,
+        generator,
+        prompt,
+        id_column_name="hash_id",
+        ):
+
+    #WARNING: don't want to handle this exception
+    if not os.path.exists(csv_path):
+        raise FileNotFoundError
+    
+    data_df = pd.read_csv(csv_path)
+    rows = list()
+    #WARNING: didn't bother handling the titles
+    for (row_idx, prototype) in enumerate(data_df[prototype_column_name]):
+        augmenteds = gen_single_augmentations(
+                prototype=prototype,
+                generator=generator,
+                prompt=prompt,
+                max_length=args.max_length_per_generated,
+                num_augmentations=args.num_augmentations_per_news,
+                )
+        rows.extend([(data_df.loc[row_idx, id_column_name], augmented) for augmented in augmenteds])
+    
+    #WARNING: naming the columns of articles to articles
+    df = pd.DataFrame(rows, columns=["hash_id", "articles"])
+    return df
+
+
+
+# main
+if __name__ == "__main__":
+    args = get_args()
+
+    #WARNING: not sure if the task option being "text-generation" is the best
+    #WARNING: not sure having access to "gpt-3.5-turbo" in an unlimited way
+    generator = pipeline(args.task_option, model=args.model_name)
+    #WARNING: This prompt is topic-based, but we can also use article-based prompts.
+    #WARNING: Should finetune the prompt
+    prompt = f"Generate a news article based on this topic: {}"
+
+    csv_path = News.csv_path
+    #WARNING: be mindful that this columns stores descriptions currently
+    prototype_column_name = News.article_column_name
+
+    #WARNING: storing data in pd.DataFrame
+    df = gen_batch_augmentations(
+            args=args,
+            csv_path = csv_path,
+            prototype_column_name=prototype_column_name,
             generator=generator,
             prompt=prompt,
             )
 
-    df = pd.DataFrame(augmenteds, columns="article")
-    df.to_csv("../test/test.csv")
+    filename = get_datetime_filename(filename_fmt="d{}.csv")
+    file_path = os.path.join(os.path.dirname(csv_path), filename)
+    df.to_csv(file_path)
