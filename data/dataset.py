bed69e2224f76b9f479ce10bcda5ca09167417c0
Yi <ym2380@nyu.edu>
2024-09-27 20:52:42 -0400

    got pair items

--- data/dataset.py
+++ data/dataset.py
@@ -19,13 +19,17 @@
 # --------------------------------------------------------------
 #                          TODOs:
 #    1. Split the dataset.
-#    2. Get articles in the database, instead of descriptions
-#    3. Current version is merely an example, actual implementa-
-#       tion requiring false news fetching and tuple pair items.
+#    2. Current version is merely an example 🪀.
+#    3. I'm still unfamiliar with the NLP task paradigm, such as
+#       the importance of the attention mask.
 # --------------------------------------------------------------
 #    Description:
 #        Dataset
 # --------------------------------------------------------------
+#    BIG WARNING:
+#        This file is just a framework and cannot be run or tes-
+#        ted because 1) real news articles are unavailable, and 
+#        2) fake news hasn't been generated yet.
 # ==============================================================
 
 
@@ -45,43 +49,84 @@ from data._gen_data import get_df
 
 # dataset
 class News(Dataset):
-    csv_path = "/scratch/ym2380/data/news/news.csv"
-    #WARNING: currently this column contains descriptions
-    article_column_name = "description"
+    real_csv_path = "/scratch/ym2380/data/news/news.csv"
+    #WARNING: Do I need to warn you that this path is fake?
+    fake_csv_path = "Ironic, as the path to fake news is itself fake."
+    #WARNING: This column name is fake. The real data lacks this column, and the fake data doesn't exsit.
+    article_column_name = "article"
     
     def __init__(self, tokenizer, max_length):
-        if not os.path.exists(self.csv_path):
+        if not os.path.exists(self.real_csv_path):
             self._download_csv()
-        self.data = pd.read_csv(self.csv_path)
+        if not os.path.exists(self.fake_csv_path):
+            raise FileNotFoundError("Error: File not found!")
+        self.real_data = pd.read_csv(self.real_csv_path)
+        self.fake_data = pd.read_csv(self.fake_csv_path)
 
         #WARNING: using pre-tokenization would be more efficient
         self.tokenizer = tokenizer
         self.max_length = max_length
 
     def __len__(self):
-        return len(self.data)
+        #WARNING: The dataset length depends on its structure. For instance, if each iteration yields 
+        #         one real and one fake news pair, and each real news has only one fake counterpart, 
+        #         the length would be len(self.real_data). However, if multiple fake news items corre-
+        #         spond to one real news, or pairs are based on topic rather than the news being real 
+        #         or fake, the length may vary. For now, we're setting the length to len(real_data) +
+        #         len(fake_real) because I'm out of my mind 🤪.
+        return len(self.real_data) + len(self.fake_data)
 
     def __getitem__(self, idx):
-        article = str(self.data[self.article_column_name][idx])
+        #WARNING: Like the dataset length, the method to fetch fake news will depend on the dataset 
+        #         structure, so I'll implement several approaches.
+        #WARNING: The following implementations are inefficient—you already know this, so I won't elaborate.
+        ikemen_kamen_amai_masuku = 1
+        if ikemen_kamen_amai_masuku == 1:
+            article_sun = self.real_data.loc[idx, self.article_column_name]
+            hash_id_ = self.real_data.loc[idx, "hash_id"]
+            article_moon = self.fake_data.loc[fake_data["hash_id"] == hash_id_, self.article_column_name].iloc[0]
+        elif ikemen_kamen_amai_masuku == 11:
+            #COMMENT: One number. What is happening?
+            eleven = 111
+            real_idx = idx // eleven
+            fake_idx = idx % eleven
+            article_sun = self.real_data.loc[real_idx, self.article_column_name]
+            hash_id_ = self.real_data.loc[real_idx, "hash_id"]
+            article_moon = self.fake_data.loc[fake_data["hash_id"] == hash_id_, self.article_column_name].iloc[fake_idx]
+        elif ikemen_kamen_amai_masuku = -1:
+            #COMMENT: skipped 🦘.
 
         #WARNING: The tokenization part deserves double-checking, as I’m not very familiar with it.
-        encoding = self.tokenizer.encode_plus(
-                article,
+        real_encoding = self.tokenizer.encode_plus(
+                real_article,
                 add_special_tokens=True,
                 max_length=self.max_length,
                 padding="max_length",
                 return_attention_mask=True,
                 return_tensors="pt",
                 )
-        input_ids = encoding['input_ids'].squeeze()
-        attention_mask = encoding['attention_mask'].squeeze()
+        real_input_ids = real_encoding['input_ids'].squeeze()
         
-        return (input_ids, attention_mask)
+        fake_encoding = self.tokenizer.encode_plus(
+                fake_article,
+                add_special_tokens=True,
+                max_length=self.max_length,
+                padding="max_length",
+                return_attention_mask=True,
+                return_tensors="pt",
+                )
+        fake_input_ids = fake_encoding['input_ids'].squeeze()
+        
+        #WARNING: attention masks should be important
+        #attention_mask = real_encoding['attention_mask'].squeeze()
+        #attention_mask = fake_encoding['attention_mask'].squeeze()
+        
+        return (real_input_ids, fake_input_ids)
     
     def _download_csv(self):
         df = get_df(db_name="rss_reader")
 
-        _csv_dir = os.path.dirname(self.csv_path)
-        if not os.path.exists(_csv_dir):
-            os.makedirs(_csv_dir)
-        df.to_csv(self.csv_file, index=False)
+        dir_ = os.path.dirname(self.real_csv_path)
+        if not os.path.exists(dir_):
+            os.makedirs(dir_)
+        df.to_csv(self.real_csv_path, index=False)
